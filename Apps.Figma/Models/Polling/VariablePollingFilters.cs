497882d7433b39a9b61d135320fe65a3c87fee43
mathijs-bb <mathijs.sonnemans@blackbird.io>
2026-09-17 18:08:12 +0200

    Updated polling event to return single output

--- Apps.Figma/Models/Polling/VariablePollingFilters.cs
+++ Apps.Figma/Models/Polling/VariablePollingFilters.cs
@@ -1,5 +1,7 @@
 ﻿using Apps.Figma.Handlers;
+using Apps.Figma.Handlers.Static;
 using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dictionaries;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.Figma.Models.Polling;
@@ -12,7 +14,12 @@ public class VariablePollingFilters
     [DataSource(typeof(VariableCollectionHandler))]
     public string CollectionId { get; set; }
 
+    [Display("Dynamic mode handling", Description = "Whether to consider the defualt or other modes only. Alternatively specify modes in the input below.")]
+    [StaticDataSource(typeof(DefaultModeHandler))]
+    public string? DefaultModeHandler { get; set; }
+
     [Display("Modes", Description = "The modes to consider. By default all modes are polled.")]
     [DataSource(typeof(VariableModeHandler))]
     public IEnumerable<string>? ModeNames { get; set; }
+
 }
